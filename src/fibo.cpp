756cac10dccdafe9a3fc248d0ee381a2eb9c26fe
Fabien Ors <fabien.ors@minesparis.psl.eu>
2024-07-23 15:13:24 +0200

    Add clang format

--- src/fibo.cpp
+++ src/fibo.cpp
@@ -6,16 +6,16 @@
 #include <cmath> // std::isnan
 
 #define DEFAULT_TITLE "Fibonacci List"
-#define LONG_SIZE 10000
+#define LONG_SIZE     10000
 
 #if defined(_WIN32) || defined(_WIN64)
-#include <windows.h> // for CreateDirectory
+#  include <windows.h> // for CreateDirectory
 #else
-#include <unistd.h> // for readlink
+#  include <unistd.h> // for readlink
 #endif
 
 #ifdef __APPLE__
-#include <mach-o/dyld.h> // for _NSGetExecutablePath
+#  include <mach-o/dyld.h> // for _NSGetExecutablePath
 #endif
 
 /*!
@@ -32,9 +32,8 @@ std::string getExecPath()
     dir = std::string(buffer);
 #elif __APPLE__
   char buffer[LONG_SIZE] = "";
-  uint32_t bufsize = LONG_SIZE;
-  if(!_NSGetExecutablePath(buffer, &bufsize))
-    dir = std::string(buffer);
+  uint32_t bufsize       = LONG_SIZE;
+  if (!_NSGetExecutablePath(buffer, &bufsize)) dir = std::string(buffer);
 #else // __linux__
   char buffer[LONG_SIZE] = "";
   if (readlink("/proc/self/exe", buffer, LONG_SIZE) != -1)
@@ -42,7 +41,6 @@ std::string getExecPath()
 #endif
   return dir;
 }
-
 
 /**
  * Return the Nth Fibonacci number, -1 in case of error
@@ -54,7 +52,8 @@ int fibn(int n)
 {
   if (n <= 0)
   {
-    std::cout << "Error in fibn: Integer argument must be strictly positive!" << std::endl;
+    std::cout << "Error in fibn: Integer argument must be strictly positive!"
+              << std::endl;
     return -1;
   }
   int a = 0;
@@ -64,8 +63,8 @@ int fibn(int n)
   {
     if (i == n) return a;
     int aa = a;
-    a = b;
-    b = aa+b;
+    a      = b;
+    b      = aa + b;
     i++;
   }
   std::cout << "Error in fibn: Unknown error!" << std::endl;
@@ -84,7 +83,8 @@ std::vector<int> fib(int n)
   std::vector<int> res;
   if (n <= 0)
   {
-    std::cout << "Error in fib: Integer argument must be strictly positive!" << std::endl;
+    std::cout << "Error in fib: Integer argument must be strictly positive!"
+              << std::endl;
     return res;
   }
   int a = 0;
@@ -93,39 +93,41 @@ std::vector<int> fib(int n)
   {
     res.push_back(a);
     int aa = a;
-    a = b;
-    b = aa+b;
+    a      = b;
+    b      = aa + b;
   }
   return res;
 }
 
 /**
  * Default constructor of a class which handle Fibonacci integers list up to n
- * 
+ *
  * @param n     Strict positive Integer
  * @param title Title to be printed (optional)
  */
 Fibo::Fibo(int n, const std::string& title)
-: _n(n)
-, _title(title)
+  : _n(n)
+  , _title(title)
 {
   if (_n <= 0)
   {
-    std::cout << "Fibonacci class must be initialized with a strict positive integer. N is set to 50." << std::endl;
+    std::cout << "Fibonacci class must be initialized with a strict positive "
+                 "integer. N is set to 50."
+              << std::endl;
     _n = 50;
   }
 
   // Test static_assert compilation
-  static_assert( true, "Class cannot be cloned as it is abstract" );
+  static_assert(true, "Class cannot be cloned as it is abstract");
   // Test isnan
   double a = 5.5;
-  if (std::isnan(a))
-    std::cout << "a is nan" << std::endl;
+  if (std::isnan(a)) std::cout << "a is nan" << std::endl;
 
   if (_title.empty())
   {
     std::stringstream sstr;
-    sstr << DEFAULT_TITLE << " (" << SWIGEX0_RELEASE << " - " << SWIGEX0_DATE << ")";
+    sstr << DEFAULT_TITLE << " (" << SWIGEX0_RELEASE << " - " << SWIGEX0_DATE
+         << ")";
     _title = sstr.str();
   }
 }
@@ -133,9 +135,7 @@ Fibo::Fibo(int n, const std::string& title)
 /**
  * Destructor
  */
-Fibo::~Fibo()
-{
-}
+Fibo::~Fibo() {}
 
 /**
  * Reset the Fibonacci list from another one
@@ -146,7 +146,7 @@ Fibo::~Fibo()
 void Fibo::resetFromFiboVal(Fibo fibo)
 {
   _title = fibo._title;
-  _n = fibo._n;
+  _n     = fibo._n;
 }
 
 /**
@@ -158,7 +158,7 @@ void Fibo::resetFromFiboVal(Fibo fibo)
 void Fibo::resetFromFiboRef(const Fibo& fibo)
 {
   _title = fibo._title;
-  _n = fibo._n;
+  _n     = fibo._n;
 }
 
 /**
@@ -168,13 +168,11 @@ void Fibo::resetFromFiboRef(const Fibo& fibo)
  */
 void Fibo::display(bool showTitle) const
 {
-  if (showTitle)
-    std::cout << _title << ": ";
+  if (showTitle) std::cout << _title << ": ";
   std::vector<int> res = getVector();
-  for (const auto& i: res)
-    std::cout << i << ' ';
+  for (const auto& i: res) std::cout << i << ' ';
   std::cout << std::endl;
-  //std::cout << "Executable path: " << getExecPath() << std::endl;
+  // std::cout << "Executable path: " << getExecPath() << std::endl;
 }
 
 /**
@@ -198,21 +196,21 @@ std::string Fibo::getTitle() const
 }
 
 #if defined(_WIN32) || defined(_WIN64)
-#include <windows.h>
-#include <io.h>
-#include <fcntl.h>
+#  include <windows.h>
+#  include <io.h>
+#  include <fcntl.h>
 #endif
 
-StdoutRedirect::StdoutRedirect(const std::string& file) :
+StdoutRedirect::StdoutRedirect(const std::string& file)
+  :
 #if defined(_WIN32) || defined(_WIN64)
   _old_stdout(0)
 #else
-  _coutbuf(nullptr),
-  _out()
+  _coutbuf(nullptr)
+  , _out()
 #endif
 {
-  if (!file.empty())
-    start(file);
+  if (!file.empty()) start(file);
 }
 
 StdoutRedirect::~StdoutRedirect()
@@ -229,10 +227,16 @@ void StdoutRedirect::start(const std::string& file)
 {
 #if defined(_WIN32) || defined(_WIN64)
   // https://stackoverflow.com/questions/54094127/redirecting-stdout-in-win32-does-not-redirect-stdout/54096218
-  _old_stdout = GetStdHandle(STD_OUTPUT_HANDLE);
-  HANDLE new_stdout = CreateFileA(file.c_str(), GENERIC_WRITE, FILE_SHARE_READ, NULL, CREATE_ALWAYS, FILE_ATTRIBUTE_NORMAL, NULL);
+  _old_stdout       = GetStdHandle(STD_OUTPUT_HANDLE);
+  HANDLE new_stdout = CreateFileA(file.c_str(),
+                                  GENERIC_WRITE,
+                                  FILE_SHARE_READ,
+                                  NULL,
+                                  CREATE_ALWAYS,
+                                  FILE_ATTRIBUTE_NORMAL,
+                                  NULL);
   SetStdHandle(STD_OUTPUT_HANDLE, new_stdout);
-  int fd = _open_osfhandle((intptr_t)new_stdout, _O_WRONLY|_O_TEXT);
+  int fd = _open_osfhandle((intptr_t)new_stdout, _O_WRONLY | _O_TEXT);
   _dup2(fd, _fileno(stdout));
   _close(fd);
 #else
@@ -250,11 +254,11 @@ void StdoutRedirect::stop()
 #if defined(_WIN32) || defined(_WIN64)
   // https://stackoverflow.com/questions/32185512/output-to-console-from-a-win32-gui-application-on-windows-10
   SetStdHandle(STD_OUTPUT_HANDLE, _old_stdout);
-  int fd = _open_osfhandle((intptr_t)_old_stdout, _O_WRONLY|_O_TEXT);
+  int fd = _open_osfhandle((intptr_t)_old_stdout, _O_WRONLY | _O_TEXT);
   if (fd >= 0) // fd could be negative for an unknown reason
   {
     FILE* fp = _fdopen(fd, "w");
-    freopen_s( &fp, "CONOUT$", "w", stdout);
+    freopen_s(&fp, "CONOUT$", "w", stdout);
   }
 #else
   std::cout.rdbuf(_coutbuf);
